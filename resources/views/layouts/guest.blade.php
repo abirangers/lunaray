2e34fec4b82e35f85e772052b3ce220c37170076
abirangers <abiayyasi12@gmail.com>
2025-11-07 20:54:02 +0700

    refactor(layout): Remove login icon from guest layout mobile view
    - Remove mobile login icon from guest layout
    - Simplify navigation by eliminating redundant login link
    - Maintain responsive design without additional login entry point

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -230,15 +230,6 @@
                                 </div>
                             </div>
                         </div>
-                    @else
-                        <div class="lg:hidden">
-                            <a href="{{ route('login') }}"
-                               class="w-11 h-11 flex items-center justify-center text-white hover:text-cyan-400 transition-colors touch-manipulation">
-                                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"/>
-                                </svg>
-                            </a>
-                        </div>
                     @endif
                 </nav>
             </header>
